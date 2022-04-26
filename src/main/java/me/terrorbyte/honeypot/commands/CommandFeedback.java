dbe583d0556f78f2d7740f29d19ed85fafe1e126
Nate Reprogle <nate.reprogle@gmail.com>
2022-04-25 22:05:48 -0500

    Switch from if to switch for better code readability and less repetition

--- src/main/java/me/terrorbyte/honeypot/commands/CommandFeedback.java
+++ src/main/java/me/terrorbyte/honeypot/commands/CommandFeedback.java
@@ -4,53 +4,76 @@ import me.terrorbyte.honeypot.ConfigColorManager;
 import org.bukkit.ChatColor;
 
 public class CommandFeedback {
-
+    
     public static String sendCommandFeedback(String feedback, Boolean... success) {
         String feedbackMessage;
         String chatPrefix = ConfigColorManager.getChatPrefix();
-
-        if (feedback.equalsIgnoreCase("usage")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Incorrect usage!\n" +
-                    chatPrefix + " " + ChatColor.WHITE + "/honeypot create [ban | kick | warn | notify | nothing]\n" +
-                    chatPrefix + " " + ChatColor.WHITE + "/honeypot remove\n" +
-                    chatPrefix + " " + ChatColor.WHITE + "/honeypot reload\n" +
-                    chatPrefix + " " + ChatColor.WHITE + "/honeypot locate");
-
-        } else if (feedback.equalsIgnoreCase("alreadyexists")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "This is already a honeypot block. If you wish to change the action, please remove and recreate it.");
-
-        } else if (feedback.equalsIgnoreCase("notapot")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.RED + "This is not a honeypot block");
-
-        } else if (feedback.equalsIgnoreCase("success")) {
-            if (success[0].equals(true)) {
-                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Successfully created honeypot!");
-
-            } else if (success[0].equals(false)) {
-                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Successfully removed honeypot!");
-
-            } else {
-                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Success!");
-
+        
+        switch(feedback.toLowerCase()){
+            case "usage" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Incorrect usage!\n" +
+                chatPrefix + " " + ChatColor.WHITE + "/honeypot create [ban | kick | warn | notify | nothing]\n" +
+                chatPrefix + " " + ChatColor.WHITE + "/honeypot remove\n" +
+                chatPrefix + " " + ChatColor.WHITE + "/honeypot reload\n" +
+                chatPrefix + " " + ChatColor.WHITE + "/honeypot locate");
             }
-        } else if (feedback.equalsIgnoreCase("nopermission")){
-            feedbackMessage = (chatPrefix + " " + ChatColor.RED + "You don't have permission to use this command");
-        } else if (feedback.equalsIgnoreCase("reload")){
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Reloading config file");
-        } else if (feedback.equalsIgnoreCase("foundpot")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Highlighting honeypot blocks within range");
-        } else if (feedback.equalsIgnoreCase("nopotfound")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "No honeypot blocks found within range");
-        } else if (feedback.equalsIgnoreCase("updateavailable")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "An update is available for this plugin. Download it at " + ChatColor.GOLD + "https://github.com/redstonefreak589/Honeypot " + ChatColor.WHITE + "for the latest features and security updates!");
-        } else if (feedback.equalsIgnoreCase("againstfilter")){
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "This block is not in the filter, so you can't do that!");
-        } else if (feedback.equalsIgnoreCase("inputcancelled")) {
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Honeypot creation cancelled");
-        } else if (feedback.equalsIgnoreCase("customactionsdisabled")){
-            feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Custom actions are disabled. Enable them in the config");
-        } else {
-            feedbackMessage = (chatPrefix + " " + ChatColor.DARK_RED + "Unknown error, please contact server admin");
+            
+            case "alreadyexists" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "This is already a honeypot block. If you wish to change the action, please remove and recreate it.");
+            }
+            
+            case "success" -> {
+                if (success[0].equals(true)) {
+                    feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Successfully created honeypot!");
+        
+                } else if (success[0].equals(false)) {
+                    feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Successfully removed honeypot!");
+        
+                } else {
+                    feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Success!");
+        
+                }
+            }
+
+            case "notapot" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.RED + "This is not a honeypot block");
+            }
+
+            case "nopermission" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.RED + "You don't have permission to use this command");
+            }
+            
+            case "reload" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Reloading config file");
+            }
+
+            case "foundpot" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Highlighting honeypot blocks within range");
+            }
+
+            case "nopotfound" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "No honeypot blocks found within range");
+            }
+            
+            case "updateavailable" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "An update is available for this plugin. Download it at " + ChatColor.GOLD + "https://github.com/redstonefreak589/Honeypot " + ChatColor.WHITE + "for the latest features and security updates!"); 
+            }
+
+            case "againstfilter" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "This block is not in the filter, so you can't do that!");
+            }
+
+            case "inputcancelled" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Honeypot creation cancelled");
+            }
+
+            case "customactionsdisabled" -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.WHITE + "Custom actions are disabled. Enable them in the config");
+            }
+
+            default -> {
+                feedbackMessage = (chatPrefix + " " + ChatColor.DARK_RED + "Unknown error, please contact server admin");
+            }
         }
 
         return feedbackMessage;
