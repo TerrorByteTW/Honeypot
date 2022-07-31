c95da9608419598fdfedd2183f11efc877c4727c
Nate Reprogle <nate.reprogle@gmail.com>
2022-07-31 17:58:54 +0000

    Made GriefPrevention check a config value for permission It didn't make sense to me to check if the player had build permission, because if they didn't GriefPrevention would've handled it anyway

--- honeypot-core/src/main/java/org/reprogle/honeypot/utils/GriefPreventionUtil.java
+++ honeypot-core/src/main/java/org/reprogle/honeypot/utils/GriefPreventionUtil.java
@@ -2,10 +2,14 @@ package org.reprogle.honeypot.utils;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.reprogle.honeypot.HoneypotConfigManager;
 
 import me.ryanhamshire.GriefPrevention.ClaimPermission;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 
+/**
+ * A small helper class for utilizing GriefPrevention
+ */
 public class GriefPreventionUtil {
 
 	/**
@@ -15,6 +19,10 @@ public class GriefPreventionUtil {
 	 * @return True if allowed, false if not
 	 */
 	public boolean isAllowed(Player player, Location location) {
+		if (Boolean.FALSE.equals(HoneypotConfigManager.getPluginConfig().getBoolean("respect-griefprevention"))) {
+			return false;
+		}
+
 		return GriefPrevention.instance.dataStore.getClaimAt(location, true, null).checkPermission(player, ClaimPermission.Build, null) == null;
 	}
 }
