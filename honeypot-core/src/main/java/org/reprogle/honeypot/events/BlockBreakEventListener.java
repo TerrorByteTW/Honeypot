e3ac44134a295b0146c2786feba0e2ca9d0a4794
Nate Reprogle <nate.reprogle@gmail.com>
2022-08-01 02:45:11 +0000

    Just syncing some small changes (comments and README updates)

--- honeypot-core/src/main/java/org/reprogle/honeypot/events/BlockBreakEventListener.java
+++ honeypot-core/src/main/java/org/reprogle/honeypot/events/BlockBreakEventListener.java
@@ -71,7 +71,7 @@ public class BlockBreakEventListener implements Listener {
                 breakAction(event);
             }
             else {
-                // If the player is not exempt, not op, and do not have remove perms, count the break. Otherwise, just
+                // If the player is not exempt, not op, and does not have remove perms, count the break. Otherwise, just
                 // activate the break action.
                 if (!event.getPlayer().hasPermission(EXEMPT_PERMISSION) && !event.getPlayer().isOp()
                         && !event.getPlayer().hasPermission(REMOVE_PERMISSION)) {
@@ -94,39 +94,43 @@ public class BlockBreakEventListener implements Listener {
         }
     }
 
-    // This is a separate event from the one above. We want to know if any Honeypots were broken due to breaking a "root" block, such as torches breaking due to
+    // This is a separate event from the one above. We want to know if any Honeypots were broken due to breaking a
+    // "root" block, such as torches breaking due to
     // the block they're on being broken
     @EventHandler(priority = EventPriority.LOW)
     public static void checkBlockBreakSideEffects(BlockBreakEvent event) {
         Block block = event.getBlock();
 
-        Block[] adjacentBlocks = new Block[] {
-            block.getRelative(BlockFace.UP),
-            block.getRelative(BlockFace.DOWN),
-            block.getRelative(BlockFace.NORTH),
-            block.getRelative(BlockFace.SOUTH),
-            block.getRelative(BlockFace.EAST),
-            block.getRelative(BlockFace.WEST)
-        };
+        Block[] adjacentBlocks = new Block[] { block.getRelative(BlockFace.UP), block.getRelative(BlockFace.DOWN),
+                block.getRelative(BlockFace.NORTH), block.getRelative(BlockFace.SOUTH),
+                block.getRelative(BlockFace.EAST), block.getRelative(BlockFace.WEST) };
 
         for (Block adjacentBlock : adjacentBlocks) {
             if (Honeypot.getHBM().isHoneypotBlock(adjacentBlock)) {
                 blockBreakEvent(new BlockBreakEvent(adjacentBlock, event.getPlayer()));
                 Honeypot.getHBM().deleteBlock(adjacentBlock);
-                Honeypot.getPlugin().getLogger().warning("A Honeypot has been removed due to the block it's attached to being broken. It was located at " + adjacentBlock.getX() + ", " + adjacentBlock.getY() + ", " + adjacentBlock.getZ() + ". "  + event.getPlayer().getName() + " is the player that indirectly broke it, so the assigned action was ran against them. If needed, please recreate the Honeypot");
-                Honeypot.getHoneypotLogger().log("A Honeypot has been removed due to the block it's attached to being broken. It was located at " + adjacentBlock.getX() + ", " + adjacentBlock.getY() + ", " + adjacentBlock.getZ() + ". "  + event.getPlayer().getName() + " is the player that indirectly broke it, so the assigned action was ran against them. If needed, please recreate the Honeypot");
+                Honeypot.getPlugin().getLogger().warning(
+                        "A Honeypot has been removed due to the block it's attached to being broken. It was located at "
+                                + adjacentBlock.getX() + ", " + adjacentBlock.getY() + ", " + adjacentBlock.getZ()
+                                + ". " + event.getPlayer().getName()
+                                + " is the player that indirectly broke it, so the assigned action was ran against them. If needed, please recreate the Honeypot");
+                Honeypot.getHoneypotLogger().log(
+                        "A Honeypot has been removed due to the block it's attached to being broken. It was located at "
+                                + adjacentBlock.getX() + ", " + adjacentBlock.getY() + ", " + adjacentBlock.getZ()
+                                + ". " + event.getPlayer().getName()
+                                + " is the player that indirectly broke it, so the assigned action was ran against them. If needed, please recreate the Honeypot");
             }
         }
     }
 
-    @SuppressWarnings({"java:S3776", "java:S2629", "java:S1192"})
+    @SuppressWarnings({ "java:S3776", "java:S2629", "java:S1192" })
     private static void breakAction(BlockBreakEvent event) {
-        
+
         // Get the block broken and the chat prefix for prettiness
         Block block = event.getBlock();
         String chatPrefix = ConfigColorManager.getChatPrefix();
 
-        // If the player isn't exempt, doesn't have permissions, or isn't Op
+        // If the player isn't exempt, doesn't have permissions, and isn't Op
         if (!(event.getPlayer().hasPermission(EXEMPT_PERMISSION) || event.getPlayer().hasPermission(REMOVE_PERMISSION)
                 || event.getPlayer().hasPermission(WILDCARD_PERMISSION) || event.getPlayer().isOp())) {
 
@@ -135,125 +139,131 @@ public class BlockBreakEventListener implements Listener {
 
             // Run certain actions based on the action of the Honeypot Block
             assert action != null;
-            Honeypot.getHoneypotLogger().log("BlockBreakEvent being called for player: " + event.getPlayer().getName() + ", UUID of " + event.getPlayer().getUniqueId() + ". Action is: " + action);
+            Honeypot.getHoneypotLogger().log("BlockBreakEvent being called for player: " + event.getPlayer().getName()
+                    + ", UUID of " + event.getPlayer().getUniqueId() + ". Action is: " + action);
             switch (action) {
-                case "kick" -> event.getPlayer().kickPlayer(chatPrefix + " " + ConfigColorManager.getConfigMessage("kick"));
+            case "kick" -> event.getPlayer().kickPlayer(chatPrefix + " " + ConfigColorManager.getConfigMessage("kick"));
 
-                case "ban" -> {
-                    String banReason = chatPrefix + " " + ConfigColorManager.getConfigMessage("ban");
+            case "ban" -> {
+                String banReason = chatPrefix + " " + ConfigColorManager.getConfigMessage("ban");
 
-                    Bukkit.getBanList(BanList.Type.NAME).addBan(event.getPlayer().getName(), banReason, null, chatPrefix);
-                    event.getPlayer().kickPlayer(banReason);
+                Bukkit.getBanList(BanList.Type.NAME).addBan(event.getPlayer().getName(), banReason, null, chatPrefix);
+                event.getPlayer().kickPlayer(banReason);
+            }
+
+            case "warn" -> event.getPlayer()
+                    .sendMessage(chatPrefix + " " + ConfigColorManager.getConfigMessage("warn"));
+
+            case "notify" -> {
+                // Notify all staff members with permission or Op that someone tried to break a honeypot block
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.hasPermission("honeypot.notify") || player.hasPermission(WILDCARD_PERMISSION)
+                            || player.isOp()) {
+                        player.sendMessage(chatPrefix + " " + ChatColor.RED + event.getPlayer().getName()
+                                + " was caught breaking a Honeypot block at x=" + block.getX() + ", y=" + block.getY()
+                                + ", z=" + block.getZ() + " in world " + block.getWorld().getName());
+                    }
                 }
 
-                case "warn" -> event.getPlayer()
-                        .sendMessage(chatPrefix + " " + ConfigColorManager.getConfigMessage("warn"));
+                Honeypot.getPlugin().getServer().getConsoleSender().sendMessage(chatPrefix + " " + ChatColor.RED
+                        + event.getPlayer().getName() + " was caught breaking a Honeypot block");
+            }
 
-                case "notify" -> {
-                    // Notify all staff members with permission or Op that someone tried to break a honeypot block
-                    for (Player player : Bukkit.getOnlinePlayers()) {
-                        if (player.hasPermission("honeypot.notify") || player.hasPermission(WILDCARD_PERMISSION)
-                                || player.isOp()) {
-                            player.sendMessage(chatPrefix + " " + ChatColor.RED + event.getPlayer().getName()
-                                    + " was caught breaking a Honeypot block at x=" + block.getX() + ", y=" + block.getY()
-                                    + ", z=" + block.getZ() + " in world " + block.getWorld().getName());
+            case "nothing" -> {
+                // Do...nothing
+            }
+
+            default -> {
+                // Default path is likely due to custom actions. Run whatever the action was
+                YamlDocument config = HoneypotConfigManager.getHoneypotsConfig();
+                if (config.contains(action)) {
+                    String type = config.getString(action + ".type");
+                    switch (type) {
+                    case "command" -> {
+                        List<String> commands = config.getStringList(action + ".commands");
+                        List<String> messages = config.getStringList(action + ".messages");
+                        if (commands.isEmpty()) {
+                            Honeypot.getPlugin().getLogger().warning(
+                                    "Commands list is empty for Honeypot type " + action + "! Please verify config");
+                            return;
+                        }
+
+                        for (String command : commands) {
+                            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),
+                                    formatCommand(command, event));
+                        }
+
+                        if (!messages.isEmpty()) {
+                            for (String message : messages) {
+                                event.getPlayer().sendMessage(formatMessage(message, event));
+                            }
                         }
                     }
 
-                    Honeypot.getPlugin().getServer().getConsoleSender().sendMessage(chatPrefix + " " + ChatColor.RED
-                            + event.getPlayer().getName() + " was caught breaking a Honeypot block");
-                }
+                    case "permission" -> {
+                        List<String> permissionsAdd = config.getStringList(action + ".permissions-add");
+                        List<String> permissionsRemove = config.getStringList(action + ".permissions-remove");
+                        List<String> messages = config.getStringList(action + ".messages");
+                        if (permissionsAdd.isEmpty() && permissionsRemove.isEmpty()) {
+                            Honeypot.getPlugin().getLogger().warning("Permissions lists are empty for Honeypot type "
+                                    + action + "! Please verify config");
+                            return;
+                        }
 
-                case "nothing" -> {
-                    // Do...nothing
-                }
+                        for (String permission : permissionsAdd) {
+                            Honeypot.getPermissions().playerAdd(null, event.getPlayer(), permission);
+                        }
 
-                default -> {
-                    // Default path is likely due to custom actions. Run whatever the action was
-                    YamlDocument config = HoneypotConfigManager.getHoneypotsConfig();
-                    if(config.contains(action)){
-                        String type = config.getString(action + ".type");
-                        switch (type) {
-                            case "command" -> {
-                                List<String> commands = config.getStringList(action + ".commands");
-                                List<String> messages = config.getStringList(action + ".messages");
-                                if(commands.isEmpty()) {
-                                    Honeypot.getPlugin().getLogger().warning("Commands list is empty for Honeypot type " + action + "! Please verify config");
-                                    return;   
-                                }
+                        for (String permission : permissionsRemove) {
+                            Honeypot.getPermissions().playerRemove(null, event.getPlayer(), permission);
+                        }
 
-                                for (String command : commands) {
-                                    Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), formatCommand(command, event));
-                                }
-
-                                if(!messages.isEmpty()) {
-                                    for (String message : messages) {
-                                        event.getPlayer().sendMessage(formatMessage(message, event));
-                                    }
-                                }
-                            }
-
-                            case "permission" -> {
-                                List<String> permissionsAdd = config.getStringList(action + ".permissions-add");
-                                List<String> permissionsRemove = config.getStringList(action + ".permissions-remove");
-                                List<String> messages = config.getStringList(action + ".messages");
-                                if(permissionsAdd.isEmpty() && permissionsRemove.isEmpty()) {
-                                    Honeypot.getPlugin().getLogger().warning("Permissions lists are empty for Honeypot type " + action + "! Please verify config");
-                                    return;   
-                                }
-
-                                for (String permission : permissionsAdd) {
-                                    Honeypot.getPermissions().playerAdd(null, event.getPlayer(), permission);
-                                }
-
-                                for (String permission : permissionsRemove) {
-                                    Honeypot.getPermissions().playerRemove(null, event.getPlayer(), permission);
-                                }
-
-                                if(!messages.isEmpty()) {
-                                    for (String message : messages) {
-                                        event.getPlayer().sendMessage(formatMessage(message, event));
-                                    }
-                                }
-
-                            }
-
-                            case "broadcast" -> {
-                                List<String> broadcasts = config.getStringList(action + ".broadcasts");
-                                List<String> messages = config.getStringList(action + ".messages");
-
-                                if(broadcasts.isEmpty()) {
-                                    Honeypot.getPlugin().getLogger().warning("Broadcasts list is empty for Honeypot type " + action + "! Please verify config");
-                                    return;   
-                                }
-
-                                for (String broadcast : broadcasts) {
-                                    Honeypot.getPlugin().getServer().broadcastMessage(formatMessage(broadcast, event));
-                                }
-
-                                if(!messages.isEmpty()) {
-                                    for (String message : messages) {
-                                        event.getPlayer().sendMessage(formatMessage(message, event));
-                                    }
-                                }
-                            }
-
-                            default -> {
-                                Honeypot.getPlugin().getLogger().warning("Honeypot " + action + " tried to run as a type that doesn't exist! Please verify config");
+                        if (!messages.isEmpty()) {
+                            for (String message : messages) {
+                                event.getPlayer().sendMessage(formatMessage(message, event));
                             }
                         }
+
+                    }
+
+                    case "broadcast" -> {
+                        List<String> broadcasts = config.getStringList(action + ".broadcasts");
+                        List<String> messages = config.getStringList(action + ".messages");
+
+                        if (broadcasts.isEmpty()) {
+                            Honeypot.getPlugin().getLogger().warning(
+                                    "Broadcasts list is empty for Honeypot type " + action + "! Please verify config");
+                            return;
+                        }
+
+                        for (String broadcast : broadcasts) {
+                            Honeypot.getPlugin().getServer().broadcastMessage(formatMessage(broadcast, event));
+                        }
+
+                        if (!messages.isEmpty()) {
+                            for (String message : messages) {
+                                event.getPlayer().sendMessage(formatMessage(message, event));
+                            }
+                        }
+                    }
+
+                    default -> {
+                        Honeypot.getPlugin().getLogger().warning("Honeypot " + action
+                                + " tried to run as a type that doesn't exist! Please verify config");
+                    }
                     }
                 }
             }
+            }
 
-        // At this point we know the player has one of those permissions above. Now we need to figure out which
+            // At this point we know the player has one of those permissions above. Now we need to figure out which
         }
         else if (event.getPlayer().hasPermission(REMOVE_PERMISSION)
                 || event.getPlayer().hasPermission(WILDCARD_PERMISSION) || event.getPlayer().isOp()) {
             event.getPlayer().sendMessage(chatPrefix + " " + ChatColor.WHITE
                     + "Just an FYI this was a honeypot. Since you broke it we've removed it");
 
-        // If it got to here, then they are exempt but can't break blocks anyway.
+            // If it got to here, then they are exempt but can't break blocks anyway.
         }
         else {
             event.setCancelled(true);
@@ -277,7 +287,8 @@ public class BlockBreakEventListener implements Listener {
         // Increment the blocks broken counter
         blocksBroken += 1;
 
-        // If the blocks broken are larger than or equals 'breaks before action' or if breaks before action equal equals 1,
+        // If the blocks broken are larger than or equals 'breaks before action' or if breaks before action equal equals
+        // 1,
         // reset the count and perform the break
         if (blocksBroken >= breaksBeforeAction || breaksBeforeAction == 1) {
             Honeypot.getHPM().setPlayerCount(event.getPlayer(), 0);
