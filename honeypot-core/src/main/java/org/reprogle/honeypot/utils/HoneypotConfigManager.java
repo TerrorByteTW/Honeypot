bf7d24cf0a8c24fdff00ec370ea8492f589b6894
Nate Reprogle <nate.reprogle@gmail.com>
2022-08-13 12:38:45 -0500

    Fixing a security hotspot found by SonarCloud

--- honeypot-core/src/main/java/org/reprogle/honeypot/utils/HoneypotConfigManager.java
+++ honeypot-core/src/main/java/org/reprogle/honeypot/utils/HoneypotConfigManager.java
@@ -45,7 +45,6 @@ public class HoneypotConfigManager extends JavaPlugin {
             config.save();
         }
         catch (IOException e) {
-            e.printStackTrace();
             plugin.getLogger().severe(
                     "Could not create/load plugin config, disabling! Please alert the plugin author with the following info: " + e);
             plugin.getPluginLoader().disablePlugin(plugin);
