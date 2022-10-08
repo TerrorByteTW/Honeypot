22d5942823aeb6f892fc5d7642c3e63a4025a16a
Nate Reprogle <nate.reprogle@gmail.com>
2022-10-08 15:33:58 -0500

    Working on permission manager to lessen spaghetti code

--- honeypot-core/src/main/java/org/reprogle/honeypot/utils/PermissionManager.java
+++ honeypot-core/src/main/java/org/reprogle/honeypot/utils/PermissionManager.java
@@ -1,5 +1,30 @@
 package org.reprogle.honeypot.utils;
 
+import org.bukkit.entity.Player;
+
+/**
+ * A utility class for better managing permissions across the plugin, instead of
+ * having to write spaghetti-logic
+ */
 public class PermissionManager {
-    
+
+    // Private constructor to hide implicit one
+    private PermissionManager() {
+
+    }
+
+    /**
+     * Checks the permission level of the player executing the action/command
+     * 
+     * @param p The {@link Player} to check
+     * @return An int representing the permission level. 0 means no permissions, 99
+     *         means Op
+     */
+    public static int checkPermissions(Player p) {
+
+        if (p.hasPermission("honeypot.*") || p.isOp())
+            return 99;
+
+        return 0;
+    }
 }
