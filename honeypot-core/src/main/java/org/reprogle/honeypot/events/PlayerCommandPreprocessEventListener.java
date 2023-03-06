7d368e2ca416a0e8cc0ae456fa5d28fe9877e718
Nate Reprogle <nate.reprogle@gmail.com>
2023-03-05 19:19:43 -0600

    Fix a bug which allows players to teleport wherever by abusing an internal command

--- honeypot-core/src/main/java/org/reprogle/honeypot/events/PlayerCommandPreprocessEventListener.java
+++ honeypot-core/src/main/java/org/reprogle/honeypot/events/PlayerCommandPreprocessEventListener.java
@@ -5,9 +5,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.reprogle.honeypot.commands.CommandFeedback;
 
 public class PlayerCommandPreprocessEventListener implements Listener {
-    
+
     /**
      * Create private constructor to hide the implicit one
      */
@@ -17,12 +18,15 @@ public class PlayerCommandPreprocessEventListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public static void playerCommandPreprocessEvent(PlayerCommandPreprocessEvent event) {
-        if (event.getMessage().startsWith("/hpteleport")) {
+        if (event.getMessage().startsWith("/hpteleport") && event.getPlayer().hasPermission("honeypot.teleport")) {
             event.setCancelled(true);
             String rawCommand = event.getMessage();
             String processedCommand = rawCommand.replace("/hpteleport", "minecraft:tp " + event.getPlayer().getName());
 
             Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), processedCommand);
+        } else {
+            event.getPlayer().sendMessage(
+                    CommandFeedback.getChatPrefix() + CommandFeedback.sendCommandFeedback("nopermission"));
         }
     }
 
